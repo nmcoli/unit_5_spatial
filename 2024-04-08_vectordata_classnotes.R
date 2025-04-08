f08ef55d5cc082747dcbd3f53c2bbf0d6e7eac96
nmcoli <ncoli@email.sc.edu>
2025-04-08 11:24:48 -0400

    class commit

---
+++ 2024-04-08_vectordata_classnotes.R
@@ -0,0 +1,53 @@
+## NMC; 2024-04-08
+# 5.2 Vector Data (Whales)
+
+
+library(sf)
+library(tidyverse)
+#library(ggplot2)
+#library(raster)
+library(mapdata)
+library(marmap)
+
+USA_crit_hab = st_read(dsn = "data/North_Atlantic_Right_Whale_Critical_Habitat/",
+                       layer = "North_Atlantic_Right_Whale_Critical_Habitat")
+head(USA_crit_hab)
+st_crs(USA_crit_hab)$epsg
+# WGS*84 = 4326
+
+# want to translate form NAD83 to WGS 84
+USA_crit_hab_sf = st_transform(USA_crit_hab, crs = 4326)
+
+carcass = read.csv("data/RW_carcasses_2017.csv")
+
+# rember we want the last coordinate to match the first coordinate to close a polygon shape
+
+CAN_crit_hab = read.csv("data/NARW_canadian_critical_habitat_2017.csv")
+head(CAN_crit_hab)
+
+Can_crit_hab_sf = CAN_crit_hab %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
+  dplyr::group_by(habitat,country) %>%
+  dplyr::summarize(do_union = FALSE) %>%
+  st_cast(to = "POLYGON")
+
+head(Can_crit_hab_sf)
+
+
+# Bind Canada and US together, adjust USA so it mirrors Canadian attributes
+
+head(USA_crit_hab_sf)
+USA_crit_hab_sf$habitat = c("GOM, SEUS")
+USA_crit_hab_sf$country = "USA"
+USA_crit_hab = USA_crit_hab_sf %>%
+  dplyr::select (country, habitat, geometry)
+
+head(USA_crit_hab_sf)
+
+crit_hab = rbind(USA_crit_hab_sf, CAN_crit_hab_sf)
+head(crit_hab)
+
+## go back and find capitalization error ##
+
+
+  
--- 2024_04_03_raster_classnotes.R
+++ 2024_04_03_raster_classnotes.R
@@ -12,7 +12,8 @@ library(tidyverse)
 library(ggplot2)
 library(raster)
 library(mapdata)
-library(marmap)
+library(marmap) #french package that allows us to access NOAA data on the web, function used --> getNOAA.bathy()
+
 
 # size of grid cell (resolution) is important
 # earth is an ellipsoid, thus 3-D on 2-D space, keep in mind your Coordinated Refernece System
@@ -107,8 +108,115 @@ GOM_chl_map = ggplot() +
   theme_bw()
 GOM_chl_map
 
-batth_m_raw = marmap::getNOAA.bathy(lon1 = lon_bounds[1], 
+bath_m_raw = marmap::getNOAA.bathy(lon1 = lon_bounds[1], 
                                     lon2 = lon_bounds[2], 
                                     lat1 = lat_bounds[1], 
                                     lat2 = lat_bounds[2],
                                     resolution = 4) # 4 arcminutes
+
+#################### End Class 2025-04-03 ###############################
+
+#HW map: 
+
+# - also shows grays reef
+# - time plot that shows month on x axis and then avg chl on y axis (either scatter or line)
+# how to axis, subset, plot it. 
+
+summary(bath_m_raw)
+
+bath_m_df = marmap:: fortify.bathy(bath_m_raw) #turns their bathy class into a dataframe / tibble. 
+head(bath_m_df) # provides x,y,z
+
+bath_m = bath_m_df %>%
+  mutate(depth_m = ifelse(z > 20, NA, z))
+
+head(bath_m)
+tail(bath_m)
+
+
+head(world_map) # recall we put the coastline in world_map from the wolrdHires map data (this is seen in geom_polygon)
+GOM_bath_map = ggplot() + geom_raster(data = bath_m, aes(x = x, y = y, fill = depth_m)) +
+  geom_polygon(data = world_map, aes(x = long, y = lat, group = group), fill = "black")+
+  coord_fixed(ratio = 1.3, xlim = lon_bounds, ylim = lat_bounds, expand = FALSE)+
+  scale_fill_gradientn(colors = c("black", "darkblue", "lightblue"), 
+                       values = scales::rescale(c(-6000, -300, 0)))
+GOM_bath_map
+
+## How to Put Contours on Maps: 
+head(bath_m)
+GOM_bath_map_contours = ggplot() +
+  geom_contour (data = bath_m, aes(x = x, y = y, z = depth_m),
+                breaks = c(-100),
+                linewidth = 0.25, 
+                colour = "white")+
+  geom_contour (data = bath_m, aes(x = x, y = y, z = depth_m),
+                breaks = c(-200),
+                linewidth = 0.25, 
+                colour = "ivory")+
+  geom_polygon(data = world_map, aes(x = long, y = lat, group = group), fill = "black")+
+  coord_fixed(ratio = 1.3, xlim = lon_bounds, ylim = lat_bounds, expand = FALSE)+
+  geom_polygon(data = world_map, aes(x = long, y = lat, group = group), fill = "black")+
+  coord_fixed(ratio = 1.3, xlim = lon_bounds, ylim = lat_bounds, expand = FALSE)+
+  scale_fill_gradientn(colors = c("black", "darkblue", "lightblue"), 
+                       values = scales::rescale(c(-6000, -300, 0)))
+  
+GOM_bath_map_contours
+## go back and check this to make sure contours and the bathymetry data are together. 
+
+## Match chl and bathy
+
+bath_m_raster = marmap::as.raster(bath_m_raw)
+
+bath_m_raster #stats on bathymetry raster 
+chl_GOM_raster # stats of chl raster data
+names(bath_m_raster) = "bath_m"
+
+# different dim, resolution (bathymetry resolution is lower) & extent, CRS is the same, 
+# line up files, with different dims, resolution and extent, and potentially CRS, though same CRS this time)
+
+
+bath_layer_chl_dims = raster:: resample(bath_m_raster, chl_GOM_raster, 
+                                        crs = crs(chl_GOM_raster))
+bath_layer_chl_dims
+
+# Now we layer
+
+raster_stack = stack(chl_GOM_raster, bath_layer_chl_dims)
+plot(raster_stack)
+
+stack_df = data.frame(raster::rasterToPoints(raster_stack))
+head(stack_df)
+
+olgio_chl_a = 0.1 # mg/m^3
+eutro_chl_a = 1.57 # mg/m^3
+
+stack_df = stack_df %>%
+  mutate(trophic_index = case_when(chl_a < olgio_chl_a ~ "olgiotrophic",
+                                   chl_a >= olgio_chl_a & chl_a < eutro_chl_a ~ "mesotrophic",
+                                   chl_a >= eutro_chl_a ~ "eutrophic")) %>%
+  mutate(trophic_index = as.factor(trophic_index))
+
+summary(stack_df)
+head(stack_df)
+
+table(stack_df$trophic_index)
+
+trohpic_status = stack_df %>%
+  filter(!is.na(trophic_index)) %>%
+  group_by(trophic_index) %>%
+  summarize(n = n(),
+            mean_depth_m = mean(layer))%>%
+  mutate(proportions = n/sum(n))
+
+
+trohpic_status
+
+# figure out why this isn't working
+ggplot()+
+  geom_boxplot(aes(x = tropic_index, y = layer), data = stack_df)
+
+# go back an finish this
+#trophic_map = ggplot() +
+ # geom_raster(data = stack_df, aes(x = x, y = y, fill = trophic_index)) + 
+  
+
